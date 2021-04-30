42b0b31dd50c5baf883245187ce253863b9f4efa
Igor Estevao <igor.1999.gigs@gmail.com>
2021-04-30 17:36:47 -0300

    adicionado login, autenticacao, crud do usuario e de cotatos

--- Gemfile
+++ Gemfile
@@ -26,8 +26,9 @@ gem 'jbuilder', '~> 2.5'
 # Use Redis adapter to run Action Cable in production
 # gem 'redis', '~> 4.0'
 # Use ActiveModel has_secure_password
-# gem 'bcrypt', '~> 3.1.7'
-
+gem 'bcrypt', '~> 3.1.7'
+# gem 'jquery-ui-rails', '~> 5.0.5'
+# gem 'rails-ujs', '~> 0.1.0'
 # Use ActiveStorage variant
 # gem 'mini_magick', '~> 4.8'
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  
+  resources :contacts
+  resources :users, only: [:new, :create, :show]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   root to: "static_pages#index"
@@ -6,5 +9,9 @@ Rails.application.routes.draw do
 
   get 'sobre', to: 'static_pages#sobre'
   get 'contato', to: 'static_pages#contato'
+  get 'entrar', to: 'sessions#new'
+  post 'entrar', to: 'sessions#create'
+  delete 'sair', to: 'sessions#destroy'
+
   
 end
